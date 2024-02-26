72c1008b52cfc46cd0a3a71e39b1fe572c9eee5b
Madison Simpson <madison_simpson@outlook.com>
2024-02-25 21:58:09 -0500

    Not completed yet

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ window.minsize(width=500, height=300)
 
 #Label
 my_label = tkinter.Label(text="Hello World", font=("Arial", 24, "bold"))
+
 my_label.pack(side="left", expand=True)
 
 window.mainloop()
