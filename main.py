452405e748dd738c6924a583cc800ff1719dab6f
Madison Simpson <madison_simpson@outlook.com>
2024-02-26 17:54:00 -0500

    COMPLETED!

--- main.py
+++ main.py
@@ -1,12 +1,38 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+def calculate():
+    miles = float(mile_entry.get())
+    km = miles * 1.609
+    val_label.config(text=km)
+
+window = Tk()
 window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+window.minsize()
+window.config(padx=20, pady=20)
 
-#Label
-my_label = tkinter.Label(text="Hello World", font=("Arial", 24, "bold"))
+# Row 1
+mile_entry = Entry()
+mile_entry.insert(END, "0")
+mile_entry.grid(column=1, row=0)
 
-my_label.pack(side="left", expand=True)
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+# Row 2
+miles_label = Label(text="is equal to")
+miles_label.grid(column=0, row=1)
+
+val_label = Label(text="0")
+val_label.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+# Row 3
+calculate_button = Button(text="Calculate", command=calculate)
+calculate_button.grid(column=1, row=2)
+
+
+
 
 window.mainloop()
